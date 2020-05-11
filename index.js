642aa72340080b5671ef96afc568b62d8993bc3d
Winston  Chung <18.chung.winston@ctkprep.org>
2020-05-11 17:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+class Polygon {
+  constructor(array) {
+    this.array = array
+  }
+  
+  get countSides() {
+    let count = 0
+    this.array.forEach(s => count = count+1)
+    return count
+  }
+  
+  get perimeter() {
+    return this.array.reduce(function(total, i) { return total + i })
+  }
+  
+}
+
+class Triangle extends Polygon {
+  
+  get isValid() {
+    
+    if (this.array.length === 3 && (this.array[0] + this.array[1]) > this.array[2] && (this.array[1] + this.array[2]) > this.array[0] && (this.array[0] + this.array[2]) > this.array[1]){return true }
+    
+    else  {return false}
+  }
+  
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (this.array.map(s => s === this.array[0])) {return true}
+    
+    else {return false}
+  }
+}
